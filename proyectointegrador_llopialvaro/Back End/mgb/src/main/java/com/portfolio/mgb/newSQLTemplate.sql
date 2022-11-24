ef7eb57f95ba611a9cd6b8d3c953b879d2969983
AlvaroLlopi <llopialvaro@gmail.com>
2022-11-24 17:11:48 -0300

    .

--- proyectointegrador_llopialvaro/Back End/mgb/src/main/java/com/portfolio/mgb/newSQLTemplate.sql
+++
@@ -1,9 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  llopi
- * Created: 30 oct 2022
- */
-
